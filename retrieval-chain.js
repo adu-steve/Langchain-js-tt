8a8f849c1f60973d341cdf089660281f0f799a85
Stephen Adu <97965000+adu-steve@users.noreply.github.com>
2024-05-21 22:15:35 +0000

    Continued with retrieval chains
    Added text splitters for splitting characters in a document

--- retrieval-chain.js
+++ retrieval-chain.js
@@ -4,7 +4,9 @@ import * as dotenv from "dotenv";
 import { Document } from "@langchain/core/documents";
 import { createStuffDocumentsChain } from "langchain/chains/combine_documents";
 import { CheerioWebBaseLoader } from "langchain/document_loaders/web/cheerio";
-
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 dotenv.config({});
 
 const model = new ChatOpenAI({
@@ -17,14 +19,20 @@ Answer the following question .
 Context :{context}
 Question:{input}`);
 
-const documentA = new Document({
-  pageContent:
-    "Study skills or study strategies are approaches applied to learning. Study skills are an array of skills which tackle the process of organizing and taking in new information, retaining information, or dealing with assessments. They are discrete techniques that can be learned, usually in a short time, and applied to all or most fields of study. More broadly, any skill which boosts a person's ability to study, retain and recall information which assists in and passing exams can be termed a study skill, and this could include time management and motivational techniques.",
+const cheerio = new CheerioWebBaseLoader(
+  "https://python.langchain.com/v0.1/docs/expression_language/"
+);
+
+const docs = await cheerio.load();
+
+const splitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 200,
+  chunkOverlap: 20,
 });
-const documentB = new Document({
-  pageContent:
-    "Due to the generic nature of study skills, they must, therefore, be distinguished from strategies that are specific to a particular field of study (e.g. music or technology), and from abilities inherent in the student, such as aspects of intelligence or learning styles. It is crucial in this, however, for students to gain initial insight into their habitual approaches to study, so they may better understand the dynamics and personal resistances to learning new techniques.",
-});
+
+const splitDocs = await splitter.splitDocuments(docs);
+
+const embeddings = new OpenAIEmbeddings();
 
 //const chain = prompt.pipe(model);
 const chain = await createStuffDocumentsChain({
@@ -32,7 +40,7 @@ const chain = await createStuffDocumentsChain({
   prompt,
 });
 const response = await chain.invoke({
-  input: "What is study skills and provide other information about it",
-  context: [documentA, documentB],
+  input: "What is LCEL?",
+  context: docs,
 });
-console.log(response);
+//console.log(docs[0].pageContent.length);
